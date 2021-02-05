91772caffb6e6e5b590022d59051f69181484b71
sowunmi mayowa <smayowa176@gmail.com>
2021-02-04 18:29:20 -0800

    handled responsiveness

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ const button = document.querySelector("#button");
 const mobileNav = document.querySelector(".mobile-nav");
 const cancel = document.querySelector(".fa-window-close");
 
-mobileNav.style.top = "96vh"
+mobileNav.style.top = "98vh"
 
 button.addEventListener("click", showMenu);
 
 function showMenu(){
-    if(mobileNav.style.top == "96vh"){
+    if(mobileNav.style.top == "98vh"){
         mobileNav.style.top = "50vh"
     }
     else{
-        mobileNav.style.top == "96vh"
+        mobileNav.style.top == "98vh"
     }
 }
 
@@ -19,6 +19,6 @@ cancel.addEventListener("click", removeMenu);
 
 function removeMenu() {
     if(mobileNav.style.top == "50vh"){
-        mobileNav.style.top = "96vh"
+        mobileNav.style.top = "98vh"
     }
 }
